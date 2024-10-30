b2e0be3c5eee5216f1a76875fcfe0cd364470444
MorTalla <118970332+BABULAH@users.noreply.github.com>
2024-10-30 23:24:09 +0000

    connexion avec l'api

--- config/db.js
+++ config/db.js
@@ -4,10 +4,8 @@ require('dotenv').config(); // Charge les variables d'environnement
 const connectDB = async () => {
     try {
         const mongoURI = process.env.MONGO_URI; // Assurez-vous que cette variable d'environnement est correctement chargée
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(mongoURI);
+
         console.log(`MongoDB connected: ${mongoURI}`);
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
